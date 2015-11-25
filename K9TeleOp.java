7e88721c237a5920f645bc7d29b248ca5c9038be
firehead2468 <centaurus.ab@gmail.com>
2015-11-25 02:19:44 -0800

    Closed control issues

--- K9TeleOp.java
+++ K9TeleOp.java
@@ -92,6 +92,7 @@ public class K9TeleOp extends OpMode {
 
 		// Initialize servo
 		servo = hardwareMap.servo.get("servo_1");
+		servo.setDirection(Servo.Direction.REVERSE);
 		
 	}
 
@@ -110,7 +111,7 @@ public class K9TeleOp extends OpMode {
 		 */
 
 		float throttle = -gamepad1.left_stick_y;
-		float direction = -gamepad1.left_stick_x;
+		float direction = gamepad1.left_stick_x;
 		float right = throttle - direction;
 		float left = throttle + direction;
 
